9f45dbf87aea9632c22e4088d5670f955ad91cba
Agathe Laburthe TOlra <agathe.laburthetolra@gmail.com>
2020-11-17 13:03:55 -0600

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :sport_sessions, only: %i[index show new create update edit destroy] do
+    resources :bookings, only: %i[create]
+  end
+  resources :bookings, only: %i[destroy] do
+    resources :reviews, only: %i[new create]
+  end
 end
