d7d506c772c7b552e70e26c78c306150b98c1e34
Christian Zamora <christianzamoraburgos@gmail.com>
2020-11-17 16:53:23 -0600

    Validations and Associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
-user1 = User.create! :email => 'chris@gmail.com', :password => '123456', :password_confirmation => '123456'
-user2 = User.create! :email => 'agathe@gmail.com', :password => '123456', :password_confirmation => '123456'
-user3 = User.create! :email => 'corentin@gmail.com', :password => '123456', :password_confirmation => '123456'
-user4 = User.create! :email => 'oscar@gmail.com', :password => '123456', :password_confirmation => '123456'
+
+user1 = User.create!(email: 'chris@gmail.com', password:  '123456', password_confirmation: '123456')
+user2 = User.create!(email: 'agathe@gmail.com', password:  '123456', password_confirmation: '123456')
+user3 = User.create!(email: 'corentin@gmail.com', password:  '123456', password_confirmation: '123456')
+user4 = User.create!(email: 'oscar@gmail.com', password:  '123456', password_confirmation: '123456')
